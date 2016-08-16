7ef2824b6c087c810b2ed6f528a55e5022263217
Rouan van der Ende <rouan@8bo.org>
2016-08-16 10:20:04 +0200

    initial

---
+++ collector/collector.js
@@ -0,0 +1,117 @@
+//record ticker data from poloniex to mongodb
+//calculate delta in 1tick, 1minute, 5minute, 1hour, 1day, 10day intervals.
+//be able to tell me what is the fastest growing share right now.
+
+
+var mongojs = require('mongojs')
+var db = mongojs('polo',["ticks"])
+
+
+
+console.log("polo collector3")
+
+
+
+/// https://poloniex.com/public?command=returnTicker
+
+var https = require('https');
+var polotick = function(cb) {
+
+	var buffer = "";
+
+	var options = {
+	  host: 'poloniex.com',
+	  port: 443,
+	  path: '/public?command=returnTicker',
+	  method: 'GET'
+	};
+
+	var req = https.request(options, function(res) {
+		res.setEncoding('utf8');
+		if (res.statusCode == 200) {
+	  		//console.log('HEADERS: ' + JSON.stringify(res.headers));		
+	  		var buffer = "";
+		} else {
+			console.log('STATUS: ' + res.statusCode);
+		}
+	  
+	  
+	  res.on('data', function (chunk) {
+	    //console.log('BODY: ' + chunk);
+	    buffer += chunk
+	  });
+
+	  res.on('end', function () {
+	  	//console.log("end.")
+	  	//console.log();
+	  	var jsontick = JSON.parse(buffer);
+	  	jsontick.timestamp = Date.now();
+	  	db.ticks.save(jsontick, function (err,res) {
+			console.log(jsontick)
+			console.log(res)
+		});
+	  })
+
+	});
+
+	req.on('error', function(e) {
+	  console.log('problem with request: ' + e.message);
+	});
+
+	// write data to request body
+	//req.write('data\n');
+	//req.write('data\n');
+	req.end();
+}
+
+polotick()
+
+
+var interval = setInterval( function() { polotick(); } , 60000 ); // 1 per minute;
+
+
+
+/*
+connection.onopen = function (session) {
+	function marketEvent (args,kwargs) {
+		//console.log(args);
+	}
+	function tickerEvent (args,kwargs) {
+		//console.log(args);
+		console.log('.');
+		var tick = {}
+		tick.currencyPair 	= args[0];
+		tick.last 			= args[1]; 
+		tick.lowestAsk 		= args[2]; 
+		tick.highestBid 	= args[3]; 
+		tick.percentChange 	= args[4]; 
+		tick.baseVolume 	= args[5]; 
+		tick.quoteVolume 	= args[6]; 
+		tick.isFrozen 		= args[7]; 
+		tick.dayHigh 		= args[8]; 
+		tick.dayLow 		= args[9]; 
+		tick.timestamp		= Date.now();
+		
+		db.ticks.save(tick, function (err,res) {
+			console.log(tick)
+			console.log(res)
+		});
+		
+
+	}
+	function trollboxEvent (args,kwargs) {
+		//console.log(args);
+	}
+	//session.subscribe('BTC_XMR', marketEvent);
+	session.subscribe('ticker', tickerEvent);
+
+	//session.subscribe('trollbox', trollboxEvent);
+}
+
+connection.onclose = function () {
+  console.log("Websocket connection closed");
+}
+		       
+connection.open();
+
+*/
---
+++ viewer/viewer.js
@@ -0,0 +1,101 @@
+// handshake.ws server
+var config = {}
+config.db = "handshake"
+//config.env = "production";
+config.env = 'development';
+
+//var env = process.env.NODE_ENV || 'development';
+//var env = 'production';
+
+
+var express   = require('express');
+var app       = express();
+var fs        = require('fs');
+var path      = require("path");
+
+
+var http = require('http')
+
+var mongojs     = require('mongojs')
+var db          = mongojs('polo',["ticks"])
+
+var cookieParser = require('cookie-parser')
+var session = require('cookie-session')
+
+var bodyParser  = require('body-parser');
+
+
+
+//app.use(compression())
+
+app.use(session({
+  name: 'session',
+  secret: "viewer_123655765876",
+  secureProxy: false,
+  maxAge: 1000*60*60*24*365*100, //100years
+}))
+
+//io.sockets.emit("arduino", data)
+
+
+
+
+
+
+
+///////////////////////////////////////////
+// WEBSERVER 
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static('static'));
+
+var apphandler = function (req, res) {
+  res.sendFile(path.join(__dirname+'/static/viewer.html'));    
+}
+
+app.get('/', apphandler);
+
+
+app.get('/api/data', function(req, res) {
+
+	db.ticks.find( {}, function (err, dbresp) {
+		var dbresptxt = JSON.stringify(dbresp)
+		//console.log(dbresp.length);
+		res.end(dbresptxt);
+	});
+
+});
+
+
+app.get('/process', function(req,res) {
+	/*
+		loads and processes all tick data
+		// options:
+		smoothing
+		samplerate
+	*/
+})
+
+
+
+httpServer = http.createServer(app);
+io = require('socket.io')(httpServer);
+httpServer.listen(8080);
+
+
+///////////////////////////////////////////
+// SOCKETS
+
+io.on('connection', function (socket) {
+  console.log("socket connected!")
+
+  socket.on('handshake', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
